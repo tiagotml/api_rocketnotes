a9415900fcbe6a1d818bca21e091efebd26b693b
Tiago Mota <Tiago Mota>
2022-11-15 16:57:21 -0300

    second

--- src/server.js
+++ src/server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get("/message/:id/:user", (request, response) => {
-    const {id,user} = request.params;
+    const { id, user } = request.params;
     response.send(`ID: ${id} User: ${user}`);
-})
+});
+
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+    response.send(`Page: ${page} Limit: ${limit}`);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
