e6c6f9a98065da6e785d0eb68aa7358a4617d495
tiagotml <45052602+tiagotml@users.noreply.github.com>
2022-11-16 10:51:10 -0300

    separando as rotas

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,7 @@
+class UsersController {
+    create(request, response) {
+        const { name, email, password } = request.body;
+        response.status(201).json({ name, email, password });
+    }
+}
+module.exports = UsersController;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,9 @@
 const express = require('express');
+const routes = require("./routes");
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params;
-    response.send(`ID: ${id} User: ${user}`);
-});
-
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query;
-    response.send(`Page: ${page} Limit: ${limit}`);
-});
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => {
